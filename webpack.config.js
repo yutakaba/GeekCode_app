112bc905b05ab1cefe616d995d973351a1d9daef
yutakaba <cgcg0222@mail2.doshisha.ac.jp>
2024-06-19 14:03:38 +0900

    Fix build configuration for Heroku

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,33 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public', 'packs'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
   mode: 'production'
 };
