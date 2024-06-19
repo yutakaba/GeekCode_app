0e910ca968e3f5583089f745cacf6384a560e46d
yutakaba <cgcg0222@mail2.doshisha.ac.jp>
2024-06-19 14:30:16 +0900

    Install missing Babel plugins and update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,17 +2,32 @@ module.exports = function(api) {
   api.cache(true);
 
   const presets = [
-    '@babel/preset-env'
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'entry',
+        corejs: 3,
+        modules: false,
+        targets: '> 0.25%, not dead',
+      },
+    ],
   ];
 
   const plugins = [
+    '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-private-methods',
-    '@babel/plugin-proposal-private-property-in-object'
+    '@babel/plugin-proposal-private-property-in-object',
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-transform-regenerator',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-transform-for-of',
   ];
 
   return {
     presets,
-    plugins
+    plugins,
   };
 };
