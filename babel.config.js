d59a3c128903e41973a7b2478445148e91a1ead5
yutakaba <cgcg0222@mail2.doshisha.ac.jp>
2024-06-19 14:16:25 +0900

    Simplify Babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,38 +1,18 @@
 module.exports = function(api) {
-  api.cache(true); // キャッシュを有効にする
+  api.cache(true);
 
   const presets = [
-    [
-      '@babel/preset-env',
-      {
-        useBuiltIns: 'entry',
-        corejs: 3,
-        modules: false,
-        targets: '> 0.25%, not dead'
-      }
-    ]
+    '@babel/preset-env'
   ];
 
   const plugins = [
-    '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-private-methods',
-    '@babel/plugin-proposal-private-property-in-object',
-    '@babel/plugin-transform-runtime',
-    '@babel/plugin-transform-regenerator',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-transform-for-of'
+    '@babel/plugin-proposal-private-property-in-object'
   ];
-
-  if (api.env('test')) {
-    plugins.push('babel-plugin-dynamic-import-node');
-  }
 
   return {
     presets,
     plugins
   };
 };
-
