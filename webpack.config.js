d947bc41a54927e0f7b25bd2d48fc753d0d049a2
yutakaba <cgcg0222@mail2.doshisha.ac.jp>
2024-08-29 18:21:20 +0900

    Adjust webpack config and install necessary Babel plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = {
+  mode: 'production', // ここを追加します
   entry: './app/javascript/packs/application.js',
   output: {
     filename: 'bundle.js',
