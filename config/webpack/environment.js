1b65b4113cd1815ba8a69a8255e64410667afa80
lntdk1121@gmail.com <lntdk1121@gmail.com>
2024-09-13 01:03:10 +0900

    Remove resolve.fallback and clean up config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,29 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
+const { VueLoaderPlugin } = require('vue-loader');
+const vue = require('./loaders/vue');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = environment
+// Vue Loader の設定
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin());
+environment.loaders.prepend('vue', vue);
+
+// mini-css-extract-plugin の設定
+environment.plugins.prepend(
+  'MiniCssExtractPlugin',
+  new MiniCssExtractPlugin({
+    filename: '[name].[contenthash].css',
+    chunkFilename: '[id].[contenthash].css'
+  })
+);
+
+// 不要なNode.js関連の設定を削除
+environment.config.delete('node.dgram');
+environment.config.delete('node.fs');
+environment.config.delete('node.net');
+environment.config.delete('node.tls');
+environment.config.delete('node.child_process');
+
+// カスタム設定の削除
+// environment.config.merge(customConfig); // この行を削除
+
+module.exports = environment;
