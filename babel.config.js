1ab76957b6aa3ed8cf223e6f2e53741dccf400c0
lntdk1121@gmail.com <lntdk1121@gmail.com>
2024-09-13 00:22:40 +0900

    Fix babel config

--- babel.config.js
+++ babel.config.js
@@ -18,7 +18,7 @@ module.exports = function(api) {
     '@babel/plugin-transform-runtime',
     '@babel/plugin-transform-regenerator',
     '@babel/plugin-transform-for-of',
-    '@babel/plugin-proposal-private-property-in-object',
+    "@babel/plugin-transform-private-property-in-object"
   ];
 
   return {
