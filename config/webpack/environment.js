819f0874dfa6bba553c52d02eaddeeb22730969b
yutakaba <lntdk1121@gmail.com>
2024-09-24 02:09:37 +0900

    Configure Babel for ES6+ transpilation

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,33 +1,29 @@
+// config/webpack/environment.js
+
 const { environment } = require('@rails/webpacker');
 const { VueLoaderPlugin } = require('vue-loader');
 const vue = require('./loaders/vue');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const babelLoader = require('./loaders/babel');
 
 // Vue Loader の設定
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin());
 environment.loaders.prepend('vue', vue);
+environment.loaders.prepend('babel', babelLoader);
 
-// mini-css-extract-plugin の設定
-environment.plugins.prepend(
-  'MiniCssExtractPlugin',
-  new MiniCssExtractPlugin({
-    filename: '[name].[contenthash].css',
-    chunkFilename: '[id].[contenthash].css'
-  })
-);
-
-environment.config.resolve.alias = {
+// `vue` のエイリアスを設定
+environment.config.set('resolve.alias', {
   'vue$': 'vue/dist/vue.esm.js'
-};
+});
 
-// 不要なNode.js関連の設定を削除
-environment.config.delete('node.dgram');
-environment.config.delete('node.fs');
-environment.config.delete('node.net');
-environment.config.delete('node.tls');
-environment.config.delete('node.child_process');
-
-// カスタム設定の削除
-// environment.config.merge(customConfig); // この行を削除
+// 不要な Node.js 関連の設定を無視
+environment.configure((webpackConfig) => {
+  webpackConfig.node = {
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty',
+  };
+});
 
 module.exports = environment;
